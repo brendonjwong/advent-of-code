55a14654967cf5a86ee86004e813917352c4ac62
brendonjwong <bjwong.w@gmail.com>
2020-12-01 22:49:43 -0800

    2018 resources and day 5 skeleton.

---
+++ src/advent_of_code/2018/day_05.clj
@@ -0,0 +1,21 @@
+(ns advent-of-code.2018.day-05
+  (:require
+    [clojure.java.io :as io]
+    [clojure.string :as str]))
+
+
+(def data
+  (->> (io/resource "2018/day-05.txt")
+       slurp
+       str/split-lines))
+
+
+(defn problem-1
+  [data])
+
+
+;;; Part 2
+
+
+(defn problem-2
+  [data])
---
+++ test/advent_of_code/2018/day_05_test.clj
@@ -0,0 +1,14 @@
+(ns advent-of-code.2018.day-05-test
+  (:require
+    [advent-of-code.2018.day-05 :as day-05]
+    [clojure.test :refer :all]))
+
+
+(deftest problem-1-test
+  (is (= ...
+         (day-04/problem-1 ...))))
+
+
+(deftest problem-2-test
+  (is (= ...
+         (day-04/problem-2 ...))))
